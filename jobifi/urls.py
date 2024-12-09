4b0fa27dc80b484591370885e1192db90bb10beb
druewolf <dwolf0305@gmail.com>
2024-12-09 18:39:11 -0500

    partial url for Authentication

--- jobifi/urls.py
+++ jobifi/urls.py
@@ -19,5 +19,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main_app.urls')), # Mounts main_app's routes at the root URL, Including main_app/urls.py allows us to manage all routes related to this app separately, keeping our code organized.
+    path('', include('main_app.urls')),
+    
+    #Authentication
+    path('/accounts/login/')
+    
 ]
--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+
+    #Authentication
+    path('accounts/register/', views.register_user, name='register_user'),
+    path('accounts/login/',),
+    path('accounts/sign_out',),
 ]
