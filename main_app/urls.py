bcc28e3ad4d6801a5aa2720688e6982a42855037
Haiaza <83097184+Haiaza@users.noreply.github.com>
2024-12-11 17:33:22 +0000

    basic views and urls setup

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('co/<str:company_name>', views.company_page, name='company-page')
+    path('companies/create/', views.company_create, name='company-create')
+    path('companies/<int:company_id>/', views.company_page, name='company-page')
+    path('companies/<int:pk>/edit/', views.company_edit, name='company-edit')
+    path('companies/<int:pk>/delete/', views.company_delete, name='company-delete')
 ]
+# | **Companies**            | `/companies/<id>/`          | `company_detail`        |
+# |                          | `/companies/create/`        | `company_create`        |
+# |                          | `/companies/<id>/edit/`     | `company_edit`          |
+# |                          | `/companies/<id>/delete/`   | `company_delete`        |
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Company
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 # Create your views here.
 
 def home(request):
@@ -12,3 +12,18 @@ def company_page(request, company_id):
     return render(request, 'company/detail.html', {
         'company': company,
     })
+
+class CompanyCreate(CreateView):
+    model = Company
+    fields = '__all__'
+    #TODO Check when im home - Possible conflict without upload functionality / logo 
+    success_url = '/companies/'
+
+class CompanyUpdate(UpdateView):
+    model = Company
+    fields = ['description', 'location', 'industry', 'website', 'logo']
+    # everything aside from the name
+
+class CompanyDelete(DeleteView):
+    model = Company
+    success_url = '/companies/'
