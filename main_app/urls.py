b5d58651e2dfee29008b25e4dc1c81e22fb6b829
druewolf <dwolf0305@gmail.com>
2024-12-10 21:35:48 -0500

    job urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,10 +8,19 @@ urlpatterns = [
     #Authentication
     path('accounts/register/', views.register_user, name='register_user'),
     path('accounts/login/', CustomLoginView.as_view(), name='login'),
-    path('accounts/logout', views.logout_view, name='logout'),
+    path('accounts/logout/', views.logout_view, name='logout'),
 
 
     #Login Pages
     path('profile/job-seeker/', views.job_seeker_profile, name='job_seeker_profile'),
     path('profile/employer/', views.employer_profile, name='employer_profile'),
+    
+    #Job Details / Read
+    path('jobs/', views.job_list, name = 'job_list'),
+    path('jobs/<int:id>/', views.job_detail, name='job_detail'),
+
+    #Job Create, Read, Delete
+    path('jobs/create/', views.job_create, name='job_create'),
+    path('jobs/<int:id>/edit', views.job_edit, name='job_edit'),
+    path('jobs/<int:id>/delete', views.job_delete, name='job_delete'),
     ]
