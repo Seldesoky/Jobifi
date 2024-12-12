f88029daca610daac7c03a91c10f11c04c8eaf58
haiaza <ajames1342@gmail.com>
2024-12-11 19:05:26 -0500

    URL Update

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,12 @@ from .views import CustomLoginView, CustomLogoutView
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('co/<str:company_name>', views.company_page, name='company-page')
+    path('companies/create/', views.CompanyCreate.as_view, name='company-create'),
+    path('companies/<int:company_id>/', views.company_page, name='company-page'),
+    path('companies/<int:pk>/edit/', views.CompanyUpdate.as_view, name='company-edit'),
+    path('companies/<int:pk>/delete/', views.CompanyDelete.as_view, name='company-delete'),
 ]
+# | **Companies**            | `/companies/<id>/`          | `company_detail`        |
+# |                          | `/companies/create/`        | `company_create`        |
+# |                          | `/companies/<id>/edit/`     | `company_edit`          |
+# |                          | `/companies/<id>/delete/`   | `company_delete`        |
