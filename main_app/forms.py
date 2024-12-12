cdc9756533eb6c58bfbd93581fc27f5f3452d2ce
haiaza <ajames1342@gmail.com>
2024-12-11 19:43:07 -0500

    Forms

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import Application
+from .models import Application, Company
 
 class ApplicationForm(forms.ModelForm):
     class Meta:
         model = Application
         fields = ['cover_letter', 'resume']
+
+
+class CompanyForm(forms.ModelForm):
+    class Meta:
+        model = Company
+        fields = '__all__'
