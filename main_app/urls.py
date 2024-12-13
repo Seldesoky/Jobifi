0df98c520f7d32ba4daa52e2c6650fe5324f4912
DrueWolf <dwolf0305@gmail.com>
2024-12-12 20:43:42 -0500

    Profile url

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,9 +10,12 @@ urlpatterns = [
     path('accounts/login/', CustomLoginView.as_view(), name='login'),
     path('accounts/logout/', views.logout_view, name='logout'),
 
-    #Login Pages
+    #Profile Pages
     path('profile/job-seeker/', views.job_seeker_profile, name='job_seeker_profile'),
+    path('profile/job-seeker/edit/', views.edit_job_seeker_profile, name='edit_job_seeker_profile'),
     path('profile/employer/', views.employer_profile, name='employer_profile'),
+    path('profile/employer/edit/', views.edit_employer_profile, name='edit_employer_profile'),
+
     
     #Job Details / Read
     path('jobs/', views.job_list, name = 'job_list'),
